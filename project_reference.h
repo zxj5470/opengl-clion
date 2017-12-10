8767896210cad7377db1c557e2ba3794a5c85cc4
zxj5470@foxmail.com <zxj5470@foxmail.com>
2017-12-10 14:22:43 +0800

    Initial commit

---
+++ project_reference.h
@@ -0,0 +1,16 @@
+//
+// Created by zh on 17-12-10.
+//
+
+#ifndef OPENGL_PROJECT_REFERENCE_H
+#define OPENGL_PROJECT_REFERENCE_H
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#endif //OPENGL_PROJECT_REFERENCE_H
